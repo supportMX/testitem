7285cbe18e7231929c172ac35d1255b60ba4d2c2
supportMX <gaomengyuan@gaomengyuandeMacBook-Pro.local>
2015-12-04 10:11:09 +0800

    TestUp

---
+++ testitem/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testitem
+//
+//  Created by 高梦缘 on 15/3/4.
+//  Copyright (c) 2015年 gmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("test11111")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
